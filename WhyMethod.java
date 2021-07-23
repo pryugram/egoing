4347a11bcba71d7dd88d89cb319b176363375ca5
pryugram <87767684+pryugram@users.noreply.github.com>
2021-07-23 18:38:55 +0900

    return; didn't learn throw yet

--- WhyMethod.java
+++ WhyMethod.java
@@ -1,23 +1,39 @@
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class WhyMethod {
 	
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 									//"a" : 인자(함수에 대입한 값), argument
-		printTwoTimes("a", "-");
-		// 100000000
-		printTwoTimes("a", "*");
-		// 100000000
-		printTwoTimes("a", "&");
-		printTwoTimes("b", "!");
-		
+				System.out.println(twoTimes("a", "-"));
+				writeFileTwoTimes("a", "*");
+				FileWriter fw = new FileWriter("out.txt");
+				fw.write(twoTimes("a", "*"));						//?
+				fw.close();
+
+		//		Email.send("egoing@a.com", "two times a", twoTimes("a", "&"));
 															
-	
-	
 	}
-																	//(메소드 밖에서 안으로)매개(하는)변수, parameter
-	public static void printTwoTimes(String text, String delimiter) { 				//delimiter: 구분자 
+	
+	public static String twoTimes(String text, String delimiter) {
+		String out = "";
+		out = out + delimiter + "\n";
+		out = out + text + "\n";
+		out = out + text + "\n";
+		return out;
+	}
+	
+	
+	public static void printTwoTimes(String text, String delimiter) { 						//(메소드 밖에서 안으로)매개(하는)변수, parameter 				
+		System.out.println(delimiter);																	//delimiter: 구분자
+		System.out.println(text);
+		System.out.println(text);
+	}
+	
+	
+	public static void writeFileTwoTimes(String text, String delimiter) { 				 
 		System.out.println(delimiter);
 		System.out.println(text);
 		System.out.println(text);
@@ -25,3 +41,8 @@ public class WhyMethod {
  
 	
 }
+
+
+
+// method: 입력값과 출력값 return
+// return의 데이터 타입을 정해야한다 
