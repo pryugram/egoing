9890acbd63b01fe01bf971e48f2732f8023c5361
pryugram <87767684+pryugram@users.noreply.github.com>
2021-07-23 09:02:54 +0900

    java_flow_control_Boolean

---
+++ BooleanApp.java
@@ -0,0 +1,20 @@
+
+public class BooleanApp {
+
+	public static void main(String[] args) {
+
+		System.out.println("One");
+		System.out.println(1);
+		
+		System.out.println(true);
+		System.out.println(false);
+		
+		String foo = "Hello world";
+		//String true = "Hello world"; true: reserved word
+		
+		
+		System.out.println(foo.contains("world"));
+		System.out.println(foo.contains("egoing"));
+	}
+
+}
