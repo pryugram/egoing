a9ffb7143b62d097953c1937a1053cb5657560eb
pryugram <87767684+pryugram@users.noreply.github.com>
2021-07-23 09:21:02 +0900

    == vs equals ?

---
+++ AuthApp.java
@@ -0,0 +1,23 @@
+
+public class AuthApp {
+
+	public static void main(String[] args) {
+
+		
+		String id = "egoing";
+		String inputId = args[0];
+		
+		System.out.println("HI.");
+			
+	//	if(inputId == id) {
+		if(inputId.equals(id)) {
+			System.out.println("Master!");
+		}
+		else {
+			System.out.println("Who are you?");
+		}
+	
+	
+	}
+
+}
