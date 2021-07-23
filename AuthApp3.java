8f551e62c04e38ab8ccb88d3582d1bc4b9a03ea6
pryugram <87767684+pryugram@users.noreply.github.com>
2021-07-23 14:16:44 +0900

    don't miss any syntax

--- AuthApp3.java
+++ AuthApp3.java
@@ -3,14 +3,22 @@ public class AuthApp3 {
 
 	public static void main(String[] args) {
 
-		String[] users = {"egoing", "jinhuck", "youbin"};
+		//String[] users = {"egoing", "jinhuck", "youbin"};
+		String [] [] users = {											// elements of users : array, array = string
+				{"egoing", "1111"},
+				{"jinhuck", "2222"},								// dont miss ','			
+				{"youbin", "3333"}
+		};																	
 		String inputId = args[0];
-		
+		String inputPass = args[1];
 		
 		boolean isLogined = false;
 		for(int i = 0; i <users.length; i++) {
-			String currentId = users[i];
-			if(currentId.equals(inputId)) {			// check if currentId == inputId
+			String[] current = users[i];
+			if(
+					current[0].equals(inputId)	&& 
+					current[1].equals(inputPass)
+				) {										// check if currentId == inputId
 				isLogined = true;
 				break; 								// break the Loop
 			}
